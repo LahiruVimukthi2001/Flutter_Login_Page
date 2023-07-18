52c9987de10abf5b94b731dee7e25ed53d41f4f1
Lahiru Vimukthi <lahiruvimukthi82@gmail.com>
2023-07-19 00:13:39 +0530

    add coloum

--- main.dart
+++ main.dart
@@ -1,37 +1,70 @@
 import 'package:flutter/material.dart';
-void main()
-{
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage(), ));
-}
-class HomePage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-        body:SafeArea(
-            child:Container(
-                width:double.infinity,
-                height: MediaQuery.of(context).size.height,
-                padding: EdgeInsets.symmetric(horizontal: 30,vertical: 50),
 
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height,
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
-                  children:<Widget>[
+                  children: <Widget>[
                     Column(
-                      children:<Widget>[
-                        Text("Welcome",style: TextStyle( fontWeight: FontWeight.bold, fontSize: 40, )),
-                        SizedBox( height:20,),
-
-                        Text("BackSlash Flutter Provides extraordinary flutter tutorials. Don subscribe!",textAlign: TextAlign.center,
-                          style: TextStyle(color:Colors.grey[900],fontSize: 15,),
+                      children: <Widget>[
+                        Text("Welcome",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40,
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          "BackSlash Flutter Provides extraordinary flutter tutorials. Don subscribe!",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.grey[900],
+                            fontSize: 15,
+                          ),
                         ),
                       ],
                     ),
-                Container(
-                      height: MediaQuery.of(context).size.height /3 ,
+                    Container(
+                      height: MediaQuery.of(context).size.height / 3,
                       decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: AssetImage("new.jpg")
-                          )
-                      ),
+                          image: DecorationImage(image: AssetImage("new.jpg"))),
                     ),
+                    Column(
+                      children: <Widget>[
+                        MaterialButton(
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: () {},
+                          color: Color(0xff0095FF),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ))));
+  }
+}
